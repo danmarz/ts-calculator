c44c6a51b8a3bc1b2ee4616604cf387aac72186b
Dan Marius Dumitrescu <dumy.tm@gmail.com>
2021-11-11 14:28:34 +0100

    done

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 "use strict";
+const parseArguments = (args) => {
+    if (args.length < 4)
+        throw new Error('Not enough arguments');
+    if (args.length > 4)
+        throw new Error('Too many arguments');
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        };
+    }
+    else {
+        throw new Error('Provided values were not numbers!');
+    }
+};
 const calculator = (a, b, op) => {
     if (isNaN(a) || isNaN(b))
         throw new Error('Input is NaN');
@@ -18,9 +33,10 @@ const calculator = (a, b, op) => {
     }
 };
 try {
-    const a = Number(process.argv[2]);
-    const b = Number(process.argv[3]);
-    console.log(calculator(a, b, 'divide'));
+    const { value1, value2 } = parseArguments(process.argv);
+    // const a: number = Number(process.argv[2]);
+    // const b: number = Number(process.argv[3]);
+    console.log(calculator(value1, value2, 'divide'));
 }
 catch (error) {
     // console.log('Something went wrong');
--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
+interface OperandValues {
+    value1: number;
+    value2: number;
+}
+
+const parseArguments = (args: Array<string>): OperandValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        };
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
 type Operation = 'multiply' | 'divide' | 'add' | 'subtract';
 
 const calculator = (a: number, b: number, op: Operation): number | string => {
@@ -19,9 +38,10 @@ const calculator = (a: number, b: number, op: Operation): number | string => {
 }
 
 try {
-    const a: number = Number(process.argv[2]);
-    const b: number = Number(process.argv[3]);
-    console.log(calculator(a, b, 'divide'));
+    const { value1, value2 } = parseArguments(process.argv);
+    // const a: number = Number(process.argv[2]);
+    // const b: number = Number(process.argv[3]);
+    console.log(calculator(value1, value2, 'divide'));
 } catch (error) {
     // console.log('Something went wrong');
     // throw error;
